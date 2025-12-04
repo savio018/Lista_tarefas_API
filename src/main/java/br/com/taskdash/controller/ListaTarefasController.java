8c103ab746f0605015a1affc4da9721533bea2e2
savio018 <saviofreitas018@gmail.com>
2025-12-03 23:14:01 -0300

    corrigindo erro no controller

--- src/main/java/br/com/taskdash/controller/ListaTarefasController.java
+++ src/main/java/br/com/taskdash/controller/ListaTarefasController.java
@@ -3,11 +3,22 @@ package br.com.taskdash.controller;
 import br.com.taskdash.model.ListaTarefas;
 import br.com.taskdash.service.ListatarefasService;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/tarefas")
-@CrossOrigin(origins = "*")
+@CrossOrigin(
+        origins = "https://savio018.github.io",      // libera só seu frontend
+        allowedHeaders = "*",
+        methods = {
+                RequestMethod.GET,
+                RequestMethod.POST,
+                RequestMethod.PUT,
+                RequestMethod.DELETE,
+                RequestMethod.OPTIONS
+        }
+)
 public class ListaTarefasController {
 
     private final ListatarefasService service;
@@ -15,24 +26,27 @@ public class ListaTarefasController {
     public ListaTarefasController(ListatarefasService service) {
         this.service = service;
     }
+
     @GetMapping
     public List<ListaTarefas> listarTodas() {
         return service.listarTodas();
     }
 
     @PostMapping
-    public ListaTarefas criarTarefa(@RequestBody ListaTarefas tarefa){
+    public ListaTarefas criarTarefa(@RequestBody ListaTarefas tarefa) {
         return service.criarTarefa(tarefa);
     }
 
     @PutMapping("/{id}/concluir")
-    public ListaTarefas concluirTarefa(@PathVariable Long id){
-    return service.concluirTarefa(id);}
+    public ListaTarefas concluirTarefa(@PathVariable Long id) {
+        return service.concluirTarefa(id);
+    }
 
     @DeleteMapping("/{id}")
-    public void deletarTarefa(@PathVariable Long id){
+    public void deletarTarefa(@PathVariable Long id) {
         service.excluirTarefa(id);
     }
 }
+
 
 
